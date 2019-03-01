75db9b701beed06382c796c58e125d349fcef757
nbstsh <nbstsh@gmail.com>
2019-03-01 12:33:58 +0900

    validate objectId middleware

--- routes/movies.js
+++ routes/movies.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const validate  = require('../middleware/validate')
+const validateObjectId = require('../middleware/validateObjectId')
 const _ = require('lodash')
 const { Movie, validateMovie } = require('../models/Movie')
 const props = ['title', 'director', 'releaseYear', 'genre', 'runningTime', 'starring', 'country']
+
 
 router.get('/',  async (req, res) => {
     const movies = await Movie.find()
@@ -19,21 +21,21 @@ router.post('/', validate(validateMovie), async (req, res) => {
     res.send(movie)
 })
 
-router.put('/:id', validate(validateMovie), async (req, res) => {
+router.put('/:id', validateObjectId, validate(validateMovie), async (req, res) => {
     const movie = await Movie.findByIdAndUpdate(req.params.id,  _.pick(req.body, props), { new: true })
     if (!movie) return res.status(404).send('Movie with given id was not found.')
 
     res.send(movie)
 })
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', validateObjectId, async (req, res) => {
     const movie = await Movie.findByIdAndDelete(req.params.id)
     if (!movie) return res.status(404).send('Movie with given id was not found.')
     
     res.send(movie)
 })
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', validateObjectId, async (req, res) => {
     const movie = await Movie.findById(req.params.id)
     if (!movie) return res.status(404).send('Movie with given id was not found')
 
