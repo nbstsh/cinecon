9b208c8521622f407647a6a97857b5c4ac22e1f0
nbstsh <nbstsh@gmail.com>
2019-03-02 18:39:27 +0900

    set Access-Control-Allow-Origin

--- startup/routes.js
+++ startup/routes.js
@@ -3,9 +3,11 @@ const movies = require('../routes/movies')
 const users = require('../routes/users')
 const auth = require('../routes/auth')
 const error = require('../middleware/error')
+const cors = require('../middleware//cors')
 
 module.exports = function(app) {
     app.use(express.json())
+    app.use(cors)
     app.use('/api/movies', movies)
     app.use('/api/users', users)
     app.use('/api/auth', auth)
