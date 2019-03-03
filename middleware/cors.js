d290305d37390f443b7739cc5635fb3e80d00db3
nbstsh <nbstsh@gmail.com>
2019-03-03 21:51:24 +0900

    add Access-Control-Allow-Methods in response header

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,7 +1,8 @@
 
 
 module.exports = (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-auth-token");
-    next();
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-auth-token')
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+    next()
 }
