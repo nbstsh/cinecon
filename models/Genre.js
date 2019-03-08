b5d9ef608fc973877780e68e9f05491cc3ac0d33
nbstsh <nbstsh@gmail.com>
2019-03-08 20:51:09 +0900

    create genre model & validator

---
+++ models/Genre.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+const Joi = require('joi')
+
+
+const genreSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        min: 1,
+        max: 50,
+        required: true
+    },
+    color: {
+        type: String,
+        match: /^#[0-9a-f]/i, // Hexadecimal (7 letters string )
+        default: '#ffffff' 
+    }
+})
+
+const Genre = mongoose.model('Genre', genreSchema)
+
+
+function validateGenre(genre) {
+    const schema = {
+        name: Joi.string().min(1).max(50).required(),
+        color: Joi.string().regex( /^#[0-9a-f]/i)
+    }
+
+    return Joi.validate(genre, schema)
+}
+
+
+exports.Genre = Genre
+exports.validateGenre = validateGenre
---
+++ routes/genres.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const router = express.Router()
+
+
+router.get('/', (req, res) => {
+    res.send('')
+})
+
+router.post('/', (req, res) => {
+    res.send('')
+})
+
+router.put('/:id', (req, res) => {
+    res.send('')
+})
+
+router.delete('/:id', (req, res) => {
+    res.send('')
+})
+
+router.get('/:id', (req, res) => {
+    res.send('')
+})
+
+
+module.exports = router
